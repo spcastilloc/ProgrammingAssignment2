5402aae0de18ab1ab073dfef35860b050ce8df2c
spcastilloc <spcastilloc@unal.edu.co>
2014-05-25 13:33:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,28 @@
-#makeCacheMatrix <- function(w = matrix()) { ##creation of special matrix object
-	inv <- NULL   ##inicialization of inverse matrix
-	set <- function(z) {  ##function of set matrix's values.##if matrix changed, then its inverse is initialized again 
-		w <<- z
-		inv <<- NULL   
-	}
-	get <- function() w	 ##return matrix
-	setinv <- function(inverse) inv <<- inverse  ##set matrix inverse
-	getinv <- function() inv ##return inverse of matrix, if matrix inverse hasn't calculated yet then return null
-	list(set = set,get = get, ##list of functions
-		setinv = setinv,
-		getinv = getinv)
+##el script permite crear un tipo especial de matriz con la función makeCacheMatrix(), este objeto puede recordar, configurar y obtener sus atributos
+##Adicionalmente permite calcular sus inversa a través de la función cacheSolve si es nnecesario
+
+makeCacheMatrix <- function(x = matrix()) { ##creación del objeto especial
+inv <- NULL ##inicialización de la inversa
+set <- function(y) { ##función para asignar valores a la matrix, inicializa de nuevo ma inversa
+x <<- y
+inv <<- NULL
+}
+get <- function() x ##retorna la matriz
+setinv <- function(inverse) inv <<- inverse ##asigna valor a la inversa
+getinv <- function() inv ##retorna la inversa de la matriz.
+list(set = set,get = get, ##lista de funciones
+setinv = setinv,
+getinv = getinv)
 }
 
-cacheSolve <- function(w) { ##function that calcule and return the inverse of special matrix 'x'
-	inv <- w$getinv() ## first get the inverse of matrix
-        if(!is.null(inv)) { ## if inverse has already calculated then return it
+cacheSolve <- function(x) { ##funcion que calcula y retorna la inversa de la matriz especial x 
+inv <- x$getinv() ## primero obtiene la inversa de la matrix
+        if(!is.null(inv)) { ## si la inversa ha sido calculada, la retorna
                 message("getting cached data")
                 return(inv)
         }
-        data <- w$get() ##if not, then get data of w 
-        inv <- solve(data) ##calcule inverse of adquired data
-        w$setinv(inv) ##set the inverse of object w
-        inv	##return the inverse
+        data <- x$get() ##si no, obtiene la matriz
+        inv <- solve(data) ##y calcula la inversa de los datos obtenidos
+        x$setinv(inv) ##configura el valor de la inversa
+        inv ##retorna la inversa
 }
